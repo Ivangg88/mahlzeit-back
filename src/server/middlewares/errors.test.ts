ec492bd2295e81f26de9d7748dfa4f750e5067a9
Ivan Garcia <ivanggv2@gmail.com>
2022-08-30 18:13:51 +0200

    Created test for the middleware generalError

--- src/server/middlewares/errors.test.ts
+++ src/server/middlewares/errors.test.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import errorNotFound from "./errors";
+import CustomError from "../../utils/error";
+import errorNotFound, { generalError } from "./errors";
 
 describe("Given a function errorNotFound", () => {
   describe("When is called", () => {
@@ -14,6 +15,28 @@ describe("Given a function errorNotFound", () => {
       errorNotFound(req as Request, res as Response);
 
       expect(res.status).toHaveBeenCalledWith(status);
+    });
+  });
+});
+
+describe("Given a function generalError.", () => {
+  describe("When is called with an error.", () => {
+    test("Then it should call the response method status with the error code and the error message.", () => {
+      const status = 500;
+      const errorText = "general error";
+      const error = new CustomError(status, errorText, "");
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as Partial<Response>;
+      const req = {} as Partial<Request>;
+      const next = () => {};
+      const errorMessage = error.publicMessage;
+
+      generalError(error, req as Request, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(status);
+      expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
     });
   });
 });
