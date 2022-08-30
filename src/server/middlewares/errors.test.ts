6212b14123846427926c7d037abec3e5b970e1b9
Ivan Garcia <ivanggv2@gmail.com>
2022-08-30 18:30:07 +0200

    Added test for the other cases at errors.test.ts

--- src/server/middlewares/errors.test.ts
+++ src/server/middlewares/errors.test.ts
@@ -20,23 +20,39 @@ describe("Given a function errorNotFound", () => {
 });
 
 describe("Given a function generalError.", () => {
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as Partial<Response>;
+  const req = {} as Partial<Request>;
+  const next = () => {};
   describe("When is called with an error.", () => {
     test("Then it should call the response method status with the error code and the error message.", () => {
       const status = 500;
       const errorText = "general error";
       const error = new CustomError(status, errorText, "");
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      } as Partial<Response>;
-      const req = {} as Partial<Request>;
-      const next = () => {};
       const errorMessage = error.publicMessage;
 
       generalError(error, req as Request, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(status);
       expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
+    });
+  });
+
+  describe("When it receives an error", () => {
+    test("Then it should call the response method status with the error code and the message 'General error'.", () => {
+      const expectedStatus = 500;
+      const expectedError = {
+        error: "General error.",
+      };
+
+      const error = new Error();
+
+      generalError(error as CustomError, req as Request, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+      expect(res.json).toHaveBeenCalledWith(expectedError);
     });
   });
 });
