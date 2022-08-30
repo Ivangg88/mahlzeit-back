9b218d464af25004b62f0b2363dd000c23fa372b
Ivan Garcia <ivanggv2@gmail.com>
2022-08-30 15:23:38 +0200

    Change the workflow sonar

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverageFrom: [
-    "src/**/*.tsx",
     "src/**/*.ts",
-    "!src/index.tsx",
     "!src/index.ts",
     "!src/loadEnvironment.ts",
     "!src/server/startServer.ts",
-    "!src/react-app-env.d.ts",
-    "!src/reportWebVitals.ts",
   ],
 };
