4e52c9da47f6e4f706c2aa7e1787de1de4b2bdc7
Ivan Garcia <ivanggv2@gmail.com>
2022-09-08 19:55:49 +0200

    Changed the name of the itemsRouters to reciptesRouters

--- src/server/routes/reciptesRouters.ts
+++ src/server/routes/reciptesRouters.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import multer from "multer";
+
 import { createReciptes, getReciptes } from "../controllers/reciptesController";
 
 const reciptesRouters = express.Router();
 
+const upload = multer({ dest: "uploads", limits: { fileSize: 3000000 } });
+
 reciptesRouters.get("/getAll", getReciptes);
-reciptesRouters.post("/create", createReciptes);
+reciptesRouters.post("/create", upload.single("image"), createReciptes);
 
 export default reciptesRouters;
