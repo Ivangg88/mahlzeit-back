106bb9ee706ea2ee6e6ba82d1df790b62e4ef89e
Ivan Garcia <ivanggv2@gmail.com>
2022-09-09 13:52:08 +0200

    Created added multer and fileStorage  to the reciptes routers

--- src/server/routes/reciptesRouters.ts
+++ src/server/routes/reciptesRouters.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import multer from "multer";
+import path from "path";
 
 import { createReciptes, getReciptes } from "../controllers/reciptesController";
+import fileStorage from "../middlewares/filesStorage";
 
 const reciptesRouters = express.Router();
-
-const upload = multer({ dest: "uploads", limits: { fileSize: 3000000 } });
+const upload = multer({
+  dest: path.join("src", "uploads"),
+  limits: { fileSize: 800000 },
+});
 
 reciptesRouters.get("/getAll", getReciptes);
-reciptesRouters.post("/create", upload.single("image"), createReciptes);
+reciptesRouters.post(
+  "/create",
+  upload.single("image"),
+  fileStorage,
+  createReciptes
+);
 
 export default reciptesRouters;
