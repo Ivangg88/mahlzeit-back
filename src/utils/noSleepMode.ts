a94bb70d8748202d16b00929937ef89c95df5b5f
Ivangg88 <you@example.com>
2024-01-22 08:56:45 +0100

    added function to keep the server on

---
+++ src/utils/noSleepMode.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import Debug from "debug";
+import chalk from "chalk";
+
+const debug = Debug("mahlzeit:noSleepMode");
+const noSleepMode = (): void => {
+  setInterval(async () => {
+    try {
+      await axios.get(
+        "https://mahlzeit-back-dev-qssa.1.us-1.fl0.io/reciptes/getAll",
+        { headers: { Connection: "keep-alive" } }
+      );
+
+      debug(chalk.greenBright("Server reanimated."));
+    } catch (error) {
+      debug(chalk.red(`Error reanimating the server.`));
+      debug(chalk.red(`Error:${error.message}`));
+    }
+  }, 36000000);
+};
+
+export default noSleepMode;
