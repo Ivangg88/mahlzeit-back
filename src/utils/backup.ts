a91d75b89f6cb6607391ed51d98055de084c61df
Ivan Garcia <ivanggv2@gmail.com>
2024-05-23 20:52:35 +0200

    updated supabase login

--- src/utils/backup.ts
+++ src/utils/backup.ts
@@ -1,6 +1,6 @@
 const backupConectionData = {
-  url: "https://ekdpoeolwqsaxezpjwon.supabase.co",
-  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZHBvZW9sd3FzYXhlenBqd29uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI3MTczODEsImV4cCI6MTk3ODI5MzM4MX0.pxbTZ46K492UY1Rkp76tjqeTMfzM0FYjbP2nF86bgYU",
+  url: process.env.SUPABASE_URL,
+  key: process.env.SUPABASE_KEY,
 };
 
 export default backupConectionData;
