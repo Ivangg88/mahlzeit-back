27f4024564826c773a1b64831745d146e0e9b4cc
Ivan Garcia <ivanggv2@gmail.com>
2025-01-26 16:57:07 +0100

    updated corsOptions

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,23 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import chalk from "chalk";
+import Debug from "debug";
 import { errorNotFound, generalError } from "./middlewares/errors";
 import reciptesRouters from "./routes/reciptesRouters";
 import userRouters from "./routes/userRouters";
 import { updateLogs } from "./middlewares/loggerStorage/loggerStorage";
 
+const debug = Debug("mahlzeit:server");
+
 const app = express();
 app.disable("x-powered-by");
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN?.split(";"),
+  origin: process.env.CORS_ORIGIN,
 };
+
+debug(chalk.blue(`Allowed origin: ${corsOptions.origin}`));
 
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
